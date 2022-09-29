f335e634c85732c884f7255ec294ffd8f07dbc27
Lucas Ryu <106638107+LucasRyuMuraoka@users.noreply.github.com>
2022-09-28 21:51:36 -0300

    Update App.js

--- appado2/V1/src/App.js
+++ appado2/V1/src/App.js
@@ -1,4 +1,10 @@
 import RouterApp from './Routes.js';
+/** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*
+ * SENAC - TADS - Programacao Web *
+ * ADO #02 Trabalhando As Rotas e LINKS *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*
+ * Nome : Lucas Ryu Muraoka e Matheus Ferreira Santos
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
 function App() {
   return(
